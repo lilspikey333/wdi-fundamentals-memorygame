1325295de3423afe66c5ee7a31d4a4e737385c6f
Jen Valladares <lilspikey333@yahoo.com>
2019-11-09 21:49:32 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,21 +2,23 @@
 const cards = ['queen', 'queen', 'king', 'king'];
 const cardsInPlay = []
 
-var cardOne = cards[1];
-
-cardsInPlay.push(cardOne);
-console.log('user flipped ' + cardOne);
-
-var cardTwo = cards[0];
-
-cardsInPlay.push(cardTwo);
-console.log('user flipped ' + cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
+const checkForMatch = function () {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");
+		}
 	}
 }
-console.log(cardsInPlay)
+
+const flipCard = function (cardId) {
+
+	console.log('User flipped ' + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
