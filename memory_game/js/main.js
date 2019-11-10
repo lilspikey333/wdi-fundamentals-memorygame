738cd9e1df00fc7e65797b943741cb26704b952e
Jen Valladares <lilspikey333@yahoo.com>
2019-11-10 00:11:34 -0600

    created gamelogic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,26 @@
 
-const cards = ['queen', 'queen', 'king', 'king'];
+const cards = [
+	{
+		rank: 'queen',
+		suit: 'hearts',
+		cardImage: 'images/queen-of-hearts.png'
+	},
+	{
+		rank: 'queen',
+		suit: 'diamonds',
+		cardImage: 'images/queen-of-diamonds.png'
+	},
+	{
+		rank: 'king',
+		suit: 'hearts',
+		cardImage: 'images/king-of-hearts.png'
+	},
+	{
+		rank: 'king',
+		suit: 'diamonds',
+		cardImage: 'images/king-of-diamonds.png'
+	}
+];
 const cardsInPlay = []
 
 const checkForMatch = function () {
@@ -14,8 +35,10 @@ const checkForMatch = function () {
 
 const flipCard = function (cardId) {
 
-	console.log('User flipped ' + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log('User flipped ' + cards[cardId].rank);
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
 
 	checkForMatch();
 }
